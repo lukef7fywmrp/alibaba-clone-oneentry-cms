5d11190e35f6e815b496e224412a08e44e75ce58
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-06-24 09:01:10 +0400

    feat: Implement login functionality

---
+++ actions/loginAction.ts
@@ -0,0 +1,60 @@
+"use server";
+
+import { IErroredResponse } from "@/lib/definitions";
+import api from "@/oneentry";
+import { redirect } from "next/navigation";
+import { IAuthPostBody } from "oneentry/dist/auth-provider/authProvidersInterfaces";
+import { cookies } from "next/headers";
+import getAPI from "@/oneentry";
+
+export default async function loginAction(prevState: any, formData: FormData) {
+  const account = formData.get("account") as string;
+  const password = formData.get("password") as string;
+
+  if (!account) {
+    return { message: "Please enter your email address" };
+  }
+
+  if (!password) {
+    return { message: "Set the login password" };
+  }
+
+  try {
+    const data: IAuthPostBody = {
+      authData: [
+        {
+          marker: "account",
+          value: account,
+        },
+        {
+          marker: "password",
+          value: password,
+        },
+      ],
+    };
+
+    const authRes = await getAPI().AuthProvider.auth("login", data);
+
+    if (!authRes?.userIdentifier) {
+      const error = authRes as unknown as IErroredResponse;
+      return {
+        message: error.message,
+      };
+    }
+
+    cookies().set("access_token", authRes.accessToken, {
+      maxAge: 60 * 60 * 24, // 24 hours
+    });
+
+    cookies().set("refresh_token", authRes.refreshToken, {
+      maxAge: 60 * 60 * 24 * 7, // 7 days
+    });
+
+    cookies().set("user_identifier", authRes.userIdentifier);
+  } catch (error) {
+    console.error(error);
+    throw new Error("Failed to login. Please try again.");
+  }
+
+  redirect("/");
+}
