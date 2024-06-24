cae6740f8a16bfb478e7eddf800bf80f1e2c1501
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-06-24 09:03:14 +0400

    chore: Add detail page component for displaying top-ranking products

---
+++ app/(app)/detail/[slug]/page.tsx
@@ -0,0 +1,16 @@
+import Banner from "@/components/Banner";
+import ProductList from "@/components/ProductList";
+
+function DetailPage({ params: { slug } }: { params: { slug: string } }) {
+  return (
+    <main className="max-w-7xl mx-auto py-12 px-6">
+      <h1 className="font-bold text-[28px] text-[#222]">
+        Top-ranking Products
+      </h1>
+      <Banner />
+      <ProductList slug={slug} />
+    </main>
+  );
+}
+
+export default DetailPage;
