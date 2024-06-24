24913f77bd440f28c6abc1ed838e45de500c26b1
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-06-24 09:02:36 +0400

    chore: Update layout to include Toaster component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { Toaster } from "@/components/ui/sonner";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={cn("flex flex-col min-h-screen", inter.className)}>
+        {children}
+        <Toaster />
+      </body>
     </html>
   );
 }
