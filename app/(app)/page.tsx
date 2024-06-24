c0b93613e63e155ecac5bf4614a9249c8228e58f
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-06-24 09:03:07 +0400

    chore: Add page component for homepage

---
+++ app/(app)/page.tsx
@@ -0,0 +1,24 @@
+import getSession from "@/actions/getSession";
+import Image from "next/image";
+import banner from "@/app/banner.png";
+import api from "@/oneentry";
+import CatalogPages from "@/components/CatalogPages";
+import CatalogHeader from "@/components/CatalogHeader";
+import { Suspense } from "react";
+
+export default async function Home() {
+  const session = await getSession();
+
+  return (
+    <main className="bg-[#F2F2F2] min-h-screen">
+      <Image src={banner} alt="Banner" className="h-[220px] object-cover" />
+
+      <div className="space-y-4 max-w-6xl mx-auto">
+        <CatalogHeader />
+        <Suspense fallback={<div>Loading...</div>}>
+          <CatalogPages />
+        </Suspense>
+      </div>
+    </main>
+  );
+}
