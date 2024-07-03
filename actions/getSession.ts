0b0c7abd90ab6299050334cef04677e57baf0829
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-03 09:05:32 +0400

    refactor: Remove unused code and update getSession function to use cookies for authentication

--- actions/getSession.ts
+++ actions/getSession.ts
@@ -2,7 +2,6 @@
 
 import api from "@/oneentry";
 import { cookies } from "next/headers";
-import { IUserEntity } from "oneentry/dist/users/usersInterfaces";
 
 interface IErroredResponse {
   statusCode: number;
@@ -11,36 +10,14 @@ interface IErroredResponse {
   pageData: null;
 }
 
-// export default async function getSession() {
-//   try {
-//     const user = await api.Users.getUser("en_US");
-
-//     console.log("user", user);
-
-//     if (!user?.id) {
-//       const error = user as unknown as IErroredResponse;
-//       return null;
-//     }
-
-//     return user;
-//   } catch (error) {
-//     console.error(error);
-//     throw new Error("Failed to get session. Please try again.");
-//   }
-// }
-
-// https://alibaba.oneentry.cloud/api/content/users/me
 export default async function getSession() {
   try {
     const access_token = cookies().get("access_token")?.value;
-    const res = await fetch(`${process.env.API_URL}/api/content/users/me`, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${access_token}`,
-        "x-app-token": `${process.env.API_TOKEN}`,
-      },
-    });
-    const user: IUserEntity = await res.json();
+
+    if (!access_token) {
+      return null;
+    }
+    const user = await api.Users.setAccessToken(access_token).getUser();
 
     if (!user?.id) {
       const error = user as unknown as IErroredResponse;
