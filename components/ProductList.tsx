053f3c4fc1d55d9913799e8b34154e3a135a1cf1
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-03 09:08:00 +0400

    refactor: Update ProductList component to use updated API endpoint for retrieving products by page URL

--- components/ProductList.tsx
+++ components/ProductList.tsx
@@ -1,8 +1,8 @@
-import getAPI from "@/oneentry";
+import api from "@/oneentry";
 import ProductCard from "./ProductCard";
 
 async function ProductList({ slug }: { slug: string }) {
-  const products = await getAPI().Products.getProductsByPageUrl(slug);
+  const products = await api.Products.getProductsByPageUrl(slug);
 
   return (
     <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-center items-center">
