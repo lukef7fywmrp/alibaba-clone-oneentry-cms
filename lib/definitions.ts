d1fbc82c77c86e63290c8a817207256a03dc33b2
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-03 09:08:26 +0400

    refactor: Update Product interface to include attributeValues for product details

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -23,3 +23,62 @@ export interface IErroredResponse {
   statusCode: number;
   message: string;
 }
+
+export type Value = {
+  plainValue: string;
+  htmlValue: string | TrustedHTML;
+};
+
+export interface Product {
+  id: number;
+  localizeInfos: {
+    title: string;
+  };
+  statusIdentifier: null;
+  statusLocalizeInfos: {};
+  attributeSetIdentifier: string;
+  position: number;
+  templateIdentifier: null;
+  shortDescTemplateIdentifier: null;
+  price: number;
+  additional: { prices: { min: number; max: number } };
+  sku: null;
+  isSync: boolean;
+  attributeValues?: {
+    productimage: {
+      type: string;
+      value: {
+        size: number;
+        filename: string;
+        previewLink: string;
+        downloadLink: string;
+      };
+      position: number;
+      isProductPreview: boolean;
+    };
+    productprice: {
+      type: string;
+      value: string;
+      position: number;
+      isProductPreview: boolean;
+    };
+    producttitle: {
+      type: string;
+      value: Value;
+      position: number;
+      isProductPreview: boolean;
+    };
+    productsubtitle: {
+      type: string;
+      value: Value;
+      position: number;
+      isProductPreview: boolean;
+    };
+    productdescription: {
+      type: string;
+      value: Value;
+      position: number;
+      isProductPreview: boolean;
+    };
+  };
+}
