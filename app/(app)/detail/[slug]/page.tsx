3dfb9a9c58683fe9a71a03cc81a01ec0feb66bd3
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:56:17 +0400

    refactor: Update DetailPage to use PopularProductBanner and ProductGrid components

--- app/(app)/detail/[slug]/page.tsx
+++ app/(app)/detail/[slug]/page.tsx
@@ -1,14 +1,12 @@
-import Banner from "@/components/Banner";
-import ProductList from "@/components/ProductList";
+import PopularProductBanner from "@/components/PopularProductBanner";
+import ProductGrid from "@/components/ProductGrid";
 
 function DetailPage({ params: { slug } }: { params: { slug: string } }) {
   return (
-    <main className="max-w-7xl mx-auto py-12 px-6">
-      <h1 className="font-bold text-[28px] text-[#222]">
-        Top-ranking Products
-      </h1>
-      {/* <Banner /> */}
-      <ProductList slug={slug} />
+    <main className="max-w-7xl mx-auto py-12 px-6 space-y-8">
+      <h1 className="font-bold text-[28px] text-[#222]">Top Ranking</h1>
+      <PopularProductBanner slug={slug} />
+      <ProductGrid slug={slug} />
     </main>
   );
 }
