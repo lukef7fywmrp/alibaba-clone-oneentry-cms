0efc5950192c7e48a7e4d9bbecbfc806ea39363c
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:57:42 +0400

    feat: Add ProductGrid component for displaying products in a grid layout

---
+++ components/ProductGrid.tsx
@@ -0,0 +1,17 @@
+import { getApiInstance } from "@/oneentry";
+import ProductCard from "./ProductCard";
+
+async function ProductGrid({ slug }: { slug: string }) {
+  const apiInstance = await getApiInstance();
+  const products = await apiInstance?.Products.getProductsByPageUrl(slug);
+
+  return (
+    <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 justify-center items-center">
+      {products?.map((product, i) => (
+        <ProductCard key={product.id} index={i} product={product} detailPage />
+      ))}
+    </section>
+  );
+}
+
+export default ProductGrid;
