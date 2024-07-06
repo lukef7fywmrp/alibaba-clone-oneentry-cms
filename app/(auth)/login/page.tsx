7bbf3231d37d6ad900fe76b172b58f99e744b4a3
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:56:21 +0400

    refactor: Update login page to use DynamicLoginForm component

--- app/(auth)/login/page.tsx
+++ app/(auth)/login/page.tsx
@@ -1,11 +1,11 @@
-import LoginForm from "@/components/LoginForm";
+import DynamicLoginForm from "@/components/DynamicLoginForm";
 import { getApiInstance } from "@/oneentry";
 
 async function LoginPage() {
   const apiInstance = await getApiInstance();
   const loginForm = await apiInstance?.Forms.getFormByMarker("signin");
 
-  return <LoginForm formEntity={loginForm} />;
+  return <DynamicLoginForm formEntity={loginForm} />;
 }
 
 export default LoginPage;
