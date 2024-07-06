231fd641a754e8952ff5af9bf90dad9f244e1e6b
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:58:38 +0400

    refactor: Rename attributeTypeToInputType to dataTypeToInputTypeMap for clarity

--- lib/definitions.ts
+++ lib/definitions.ts
@@ -1,23 +1,22 @@
 import { HTMLInputTypeAttribute } from "react";
 
-export const attributeTypeToInputType: Record<string, HTMLInputTypeAttribute> =
-  {
-    string: "text",
-    text: "text",
-    textWithHeader: "text",
-    integer: "number",
-    real: "number",
-    float: "number",
-    dateAndTime: "datetime-local",
-    date: "date",
-    time: "time",
-    file: "file",
-    image: "file",
-    groupOfImages: "file",
-    radioButton: "radio",
-    list: "radio",
-    button: "button",
-  };
+export const dataTypeToInputTypeMap: Record<string, HTMLInputTypeAttribute> = {
+  string: "text",
+  text: "text",
+  textWithHeader: "text",
+  integer: "number",
+  real: "number",
+  float: "number",
+  dateAndTime: "datetime-local",
+  date: "date",
+  time: "time",
+  file: "file",
+  image: "file",
+  groupOfImages: "file",
+  radioButton: "radio",
+  list: "radio",
+  button: "button",
+};
 
 export interface IErroredResponse {
   statusCode: number;
