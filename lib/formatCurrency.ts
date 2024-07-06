24922014885979a44ed978fdcb7148976b7be106
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:58:41 +0400

    feat: Add formatCurrency utility function for formatting currency values

---
+++ lib/formatCurrency.ts
@@ -0,0 +1,8 @@
+function formatCurrency(value: number) {
+  return value.toLocaleString(undefined, {
+    minimumFractionDigits: 2,
+    maximumFractionDigits: 2,
+  });
+}
+
+export default formatCurrency;
