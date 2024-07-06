81c2184575b1ce0b0df27497448222fbb7a43876
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:56:11 +0400

    refactor: Update cart page layout to use larger gap between items

--- app/(app)/cart/page.tsx
+++ app/(app)/cart/page.tsx
@@ -5,7 +5,7 @@ async function CartPage() {
   return (
     <div className="p-5 sm:px-10">
       <h1 className="text-[28px] text-[#222] font-bold">Shopping cart</h1>
-      <div className="flex flex-col lg:flex-row items-start gap-8">
+      <div className="flex flex-col lg:flex-row items-start gap-10">
         <ItemList />
         <OrderSummary />
       </div>
