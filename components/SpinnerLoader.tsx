b7f587c395d2414607565763a8bbe8bb44b070a9
lukef7fywmrp <lukef7fywmrp@gmail.com>
2024-07-07 02:58:27 +0400

    feat: Add SpinnerLoader component for displaying a loading spinner

---
+++ components/SpinnerLoader.tsx
@@ -0,0 +1,7 @@
+export default function SpinnerLoader() {
+  return (
+    <div className="flex items-center justify-center h-72">
+      <div className="animate-spin rounded-full border-4 border-primary border-t-transparent h-12 w-12" />
+    </div>
+  );
+}
